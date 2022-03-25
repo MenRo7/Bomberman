6e4d7081a80cea070fd026050f2b0eec1ffa4367
MenRo7 <romain.garnier1@etudiant.univ-reims.fr>
2022-03-25 22:13:17 +0100

    Collision version 2

--- src/includes/exceptions/BombermanException.h
+++ src/includes/exceptions/BombermanException.h
@@ -7,9 +7,12 @@
 class BombermanException: public std::exception
 {
 	public:
-		BombermanException(std::string error) noexcept;
+		BombermanException(std::string error): m_error(error){};
 
-		virtual const char* what() const noexcept;
+		virtual const char* what() const throw()
+		{
+			return m_error.c_str();
+		}
 	
 	private:
 		std::string m_error;
