f43ff4bdab819df6825438f22dbfcf1288d910cb
MenRo7 <romain.garnier1@etudiant.univ-reims.fr>
2022-10-28 09:14:26 +0200

    cleaning

--- Bomberman/src/includes/exceptions/BombermanException.h
+++ Bomberman/src/includes/exceptions/BombermanException.h
@@ -1,21 +1,50 @@
+/**
+ * \file BombermanException.h
+ * \author Romain GARNIER
+ * \brief Fichier de la classe définissant une exception de type BombermanException
+ * \version 1.0
+*/
 #ifndef __BOMBERMANEXCEPTION__
 #define __BOMBERMANEXCEPTION__
 
 #include <exception>
 #include <string>
 
+/**
+ * \class BombermanException
+ * \author Romain GARNIER
+ * \brief Définition d'une exception BombermanException
+ * 
+ * \sa Exception
+ * 
+ * Hérite de la classe Exception
+*/
+
 class BombermanException: public std::exception
 {
 	public:
+		/**
+         * \brief Constructeur par initialisation de BombermanException
+		 * 
+         * \param error message d'erreur
+         * 
+         */
 		BombermanException(std::string error): m_error(error){};
 
+		/**
+         * \brief Renvoie de la cause de l'exception
+		 * 
+         * \return char* le message d'erreur
+		 * 
+         * \author Romain GARNIER
+         */
 		virtual const char* what() const throw()
 		{
 			return m_error.c_str();
 		}
 	
 	private:
-		std::string m_error;
+		std::string m_error; /*!< message d'erreur de l'exception*/
 
 };
 
